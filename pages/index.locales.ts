d4f701662ca3b8de0c086ac1bbf762854b6826c1
Wagner Kramer <wagner@kramer.dev.br>
2022-04-13 14:38:35 -0300

    feat: jsonLD + keywords

---
+++ pages/index.locales.ts
@@ -0,0 +1,18 @@
+const locales = {
+  'pt-BR': {
+    description:
+      'KramerDev é o portfolio de Wagner J. Kramer Vieira, um estudante de Ciência da Computação e Desenvolvedor Full Stack Brasileiro.',
+    keywords:
+      'KramerDev, Wagner J. Kramer Vieira, Desenvolvedor, Full Stack, Ciência da Computação, Portfolio, Wagner, Kramer, Wagner Kramer',
+    jobTitle: 'Desenvolvedor Full Stack',
+  },
+  'en-US': {
+    description:
+      'KramerDev is the portfolio of Wagner J. Kramer Vieira, Computer Science Student and Full Stack Developer based in Brazil.',
+    keywords:
+      'KramerDev, Wagner J. Kramer Vieira, Full Stack, Computer Science, Student, Developer, Portfolio, Wagner, Kramer, Wagner Kramer',
+    jobTitle: 'Full Stack Developer',
+  },
+}
+
+export default locales
---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Banner from '../components/Banner/Banner'
+import About from '../components/About/About'
+import Projects from '../components/Projects/Projects'
+import Contact from '../components/Contact/Contact'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import locales from './index.locales'
+
+const Home = () => {
+  const { locale } = useRouter()
+  return (
+    <>
+      <Head>
+        <title>KramerDev</title>
+        <meta name="description" content={locales[locale].description} />
+        <meta name="keywords" content={locales[locale].keywords} />
+        <script type="application/ld+json">
+          {`{
+          "@context": "http://schema.org/",
+          "@type": "Person",
+          "name": "Wagner Kramer",
+          "jobTitle": "${locales[locale].jobTitle}",
+          "telephone": "+55 (41) 9 9984-6408",
+          "url": "https://kramer.dev.br",
+          "sameAs": [
+            "https://www.linkedin.com/in/wagnerkramer/",
+            "https://www.instagram.com/kramer.wagner/",
+            "https://www.researchgate.net/profile/Wagner-Kramer"
+          ]
+        }`}
+        </script>
+      </Head>
+      <Navbar />
+      <Banner />
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
+export default Home
